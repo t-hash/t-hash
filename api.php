48de3fe76fe4a5b42b9601936335bad5371cecce
earth-faucet <w-fuller@live.com>
2014-01-07 19:52:06 +1300

    1

--- api.php
+++ api.php
@@ -2,11 +2,9 @@
 require_once 'jsonRPCClient.php';
 
 $litecoin = new jsonRPCClient ('http://wfuller:Xadsl3612@http://192.81.210.142:9332/');
-$megacoin = new jsonRPCClient ('http://wfuller:Xadsl3612@http://192.81.210.142:9333/');
-$earthcoin = new jsonRPCClient ('http://wfuller:Xadsl3612@http://192.81.210.142:9334/');
-$worldcoin = new jsonRPCClient ('http://wfuller:Xadsl3612@http://192.81.210.142:9335/');
 
-$ltcdiff = ($litecoin->getdifficulty());
-$mecdiff = ($megacoin->getdifficulty());
+
+$ltcdiff = echo ($litecoin->getdifficulty());
+
 
 ?>
